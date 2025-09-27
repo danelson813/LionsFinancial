7efcf1ca866aed08fc39e29ec3aaf866b4df08a0
Dan Nelson <44625150+danelson813@users.noreply.github.com>
2025-09-27 13:59:45 -0700

    added logging

---
+++ helpers/__init__.py
@@ -0,0 +1,16 @@
+# LionsFinancial/helpers/__init__.py
+import sys
+from loguru import logger
+logger.remove(0)
+logger.add(sys.stderr,
+           format="{time:HH:mm:ss} | {level} | {message}",
+           level='INFO')
+logger.add('logs/app.log',
+        level="INFO",
+        format="{time:HH:mm:ss} | {level} | {message}",
+        )
+
+logger.info("logger has started")
+
+
+
---
+++ main.py
@@ -0,0 +1,8 @@
+import openpyxl
+import duckdb
+import pandas as pd
+from pathlib import Path
+from loguru import logger
+
+from helpers.util import *
+
