a5499e3f8a33195fdd1db3ca9530931611982faf
Dan Nelson <44625150+danelson813@users.noreply.github.com>
2025-09-28 17:29:00 -0700

    added seperating inc and exp

--- helpers/util.py
+++ helpers/util.py
@@ -1,2 +1,48 @@
+import pandas as pd
+import duckdb
+from pathlib import Path
+
 def does_nothing():
     pass
+
+
+def deal_with_duckdb(df_: pd.DataFrame, path: Path):
+    conn = duckdb.connect(path)
+    q = """
+        CREATE OR REPLACE TABLE row AS SELECT * FROM df_;
+    """
+
+    conn.execute(q)
+    conn.commit()
+    conn.close()
+
+
+def check_out_db(path: Path) -> None:
+    conn = duckdb.connect(path)
+    q = """
+      SELECT payee, amount, budget_line, budget FROM row;  
+    """
+    df_ = conn.sql(q).to_df()
+    conn.commit()
+    conn.close()
+    print(df_.head())
+
+
+def separate_inc(path: Path) -> pd.DataFrame:
+    conn = duckdb.connect(path)
+    q = """
+        SELECT payee, amount, budget_line, budget FROM row
+        WHERE exp = 1;    
+    """
+    df_income = conn.sql(q).to_df()
+    return df_income
+
+
+def separate_exp(path: Path) -> pd.DataFrame:
+    conn = duckdb.connect(path)
+    q = """
+          SELECT payee, amount, budget_line, budget FROM row
+          WHERE exp = 0;    
+      """
+    df_exp = conn.sql(q).to_df()
+    return df_exp
--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 # LionsFinancial/main.py
 
-from helpers.util import does_nothing
+import pandas as pd
+from pathlib import Path
 
-does_nothing()
+from helpers.util import check_out_db
+from helpers.util import deal_with_duckdb
+from helpers.util import separate_inc
+from helpers.util import separate_exp
+
+
+ledger_path = Path.cwd() / "data/Ledger 25.xlsx"
+database_path = Path("/Users/dannelson/Desktop/LionsFinancial_") / "data/lions.db"
+df = pd.read_excel(ledger_path, usecols='A:H', index_col=0)
+df.columns = ['date', 'payee', 'check#', 'exp', 'amount', 'budget_line', 'budget']
+print(df.info())
+deal_with_duckdb(df, database_path)
+
+# separate by plus or minus
+df_inc = separate_inc(database_path)
+df_exp = separate_exp(database_path)
+print(df_exp.head())
+print("*"*50)
+print(df_inc.head())
